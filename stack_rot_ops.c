a9288b50bc9723df55c758c70f2ceed1500b8361
thisisteey <dadataiwo82@yahoo.com>
2023-11-15 14:24:41 -0800

    File containing stack rotation operations

---
+++ stack_rot_ops.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * stk_rotl - rotates the stack to the top
+ * @top: double pointer to the head of the stack
+ * @count: line number in the monty file
+ * Return: void
+ */
+void stk_rotl(stack_t **top, unsigned int count)
+{
+	(void)count;
+	stack_t *tmpnode = *top, *currhead;
+
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		return;
+	}
+	currhead = (*top)->next;
+	currhead->prev = NULL;
+	while (tmpnode->next)
+	{
+		tmpnode = tmpnode->next;
+	}
+	tmpnode->next = *top;
+	(*top)->next = NULL;
+	(*top)->prev = tmpnode;
+	(*top) = currhead;
+}
+
+/**
+ * stk_rotr - rotates the stack to the bottom
+ * @top: double pointer to the head of the stack
+ * @count: line number in the monty file
+ * Return: void
+ */
+void stk_rotr(stack_t **top, unsigned int count)
+{
+	(void)count;
+	stack_t *cpnode;
+
+	cpnode = *top;
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		return;
+	}
+	while (cpnode->next)
+	{
+		cpnode = cpnode->next;
+	}
+	cpode->next = *top;
+	cpnode->prev->next = NULL;
+	cpnode->prev = NULL;
+	(*top)->prev = cpnode;
+	(*top) = cpnode;
+}
